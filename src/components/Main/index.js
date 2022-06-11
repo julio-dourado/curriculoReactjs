b4ecc17499135cf302fa8a365f6a4786aaeea732
Júlio César <juliodourado419@gmail.com]>
2022-06-11 00:52:46 -0300

    3

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -29,56 +29,6 @@ export default function Main() {
             </div>
           </section>
 
-          <div class='Line' id='experience'></div>
-
-          <section class='Content experience'>
-            <h1 class='Title experience'>
-              EXPERIÊNCIA
-            </h1>
-
-            <section class='ExperienceContent'>
-              <section class='ExperienceTitle'>
-                <div class='ExperienceJob'>Projeto de APC</div>
-                <div class='ExperienceDate'>Julho 2021 - Novembro 2021</div>
-              </section>
-              <div class='ExperiencePlace'>Projeto de APC</div>
-              <div class='ExperienceDescription'>
-                Projeto sobre manipulação de dados e geração de gráficos sobre as Olimpíadas
-                utilizando Python com Plotly, Dash e Pandas. 
-                <a href="https://github.com/orgs/UNB-APC/repositories">https://github.com/orgs/UNB-APC/repositories</a>
-
-              </div>
-
-              <div class='ExperienceJob'>API todo list</div>
-              <div class='ExperienceDescription'>
-                API criada para cadastrar  diferentes licenças usuários como Premium ou Comum, adicionar 'to do' e marcar 'to do' como feito.
-                <a href="https://github.com/typejulio/desafio1-nodejs-todoslist">https://github.com/typejulio/desafio1-nodejs-todoslist</a>
-
-              </div>
-
-
-              <div class='ExperiencePlace'>Camara dos Deputados</div>
-              <div class='ExperienceDate'>Janeiro 2018 - Dezembro 2020</div>
-              <div class='ExperienceDescription'>
-                Atuei como jovem aprendiz na area administrativa do Pró-saúde, onde atualizava cadastros de planos de saude
-                de deputados e seus dependentes
-              </div>
-
-              <section class='ExperienceTitle others'>
-                <div class='ExperienceJob'>Assessor de Projetos</div>
-                <div class='ExperienceDate'>Janeiro 2021 - Atualmente</div>
-              </section>
-              <div class='ExperiencePlace'>Orc’estra Gamificação</div>
-              <div class='ExperienceDescription'>
-                Como assesor de projetos, participo diretamente na gerencia dos projetos da Orc'estra,
-                na capacitação dos membros em tecnologias e na organização do processo trainee.
-              </div>
-            </section>
-
-          </section>
-
-          <div class='Line' id='socialMedia'></div>
-
           <section class='Content socialMedia'>
             <h1 class='Title'>
               Redes Sociais
@@ -97,11 +47,6 @@ export default function Main() {
               <a href='https://www.instagram.com/juliorx_/' class='SocialCard'>
                 <img src="assets/instagramIcon.png" alt="Linkedin" class='Icon' />
                 <div class='SocialName'>@juliorx_</div>
-              </a>
-
-              <a href='https://twitter.com/nai104' class='SocialCard'>
-                <img src="assets/twitterIcon.png" alt="Linkedin" class='Icon' />
-                <div class='SocialName'>@julio_dourado</div>
               </a>
             </section>
           </section>
